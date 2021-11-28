dd6a2dd8928184d90bd3d6b2d21a0fad93b50d58
Sourabh Shrivastava <sourabhshrivastava33@gmail.com>
2021-11-28 17:23:18 +0530

    Updated Gateway cmd

---
+++ src/main/java/com/app/libraryapigateway/filter/PreFilter.java
@@ -0,0 +1,41 @@
+package com.app.libraryapigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Component
+public class PreFilter extends ZuulFilter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PreFilter.class);
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        LOGGER.info("Pre Filter: request to: {}", request.getRequestURI());
+        // handle JwtToken logic
+        return null;
+    }
+}
